68547dedf257954aed1750d034109d82da2cbbee
Pro gupta <pchemistry.35@gmail.com>
2025-04-03 12:04:37 +0530

    chore: init

---
+++ dodopayments.js
@@ -0,0 +1,21 @@
+const DodoPayments = require('dodopayments');
+
+class DodoPaymentWrapper {
+    constructor() {
+        this.client =  new DodoPayments({
+            bearerToken: process.env['DODO_PAYMENTS_API_KEY'], // This is the default and can be omitted
+        });
+    }
+
+    async getAllPurchases() {
+        try {
+            const payment = await this.client.payments.list({ page_number: 100, status: 'succeeded' });
+            return payment.items.length;
+        } catch (error) {
+            console.error('Error getting payment:', error);
+            throw error;
+        }
+    }
+}
+
+module.exports = DodoPaymentWrapper;
---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const _ = require('lodash');
+const { Webhook } = require("standardwebhooks");
+const DodoPaymentWrapper = require('./dodopayments');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const pushContactToEmailer = async (name, email, group, status, link) => {
+    fetch(`${process.env.EZ_EMAILER_URL}/api/contacts`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.EZ_EMAILER_TOKEN}`
+        },
+        body: JSON.stringify({ name, email, group, status, link })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+} 
+
+// Webhook for payment success
+app.post('/dodopayment/webhook', async (req, res) => {
+  try {
+    const webhook = new Webhook(process.env.DODO_WEBHOOK_SECRET);
+
+    const webhookHeaders = {
+        "webhook-id": (req.headers["webhook-id"] || ""),
+        "webhook-signature": (req.headers["webhook-signature"] || ""),
+        "webhook-timestamp": (req.headers["webhook-timestamp"] || ""),
+      };
+      
+    const raw = JSON.stringify(req.body);
+    const samePayloadOutput = await webhook.verify(raw, webhookHeaders);
+
+    if(_.isEqual(samePayloadOutput, req.body)) {
+        let responseData = req.body;
+
+        if (responseData.payment_id) {
+            await pushContactToEmailer('Customer', email, 'customer', 'unsubscribed', '');
+        } else {
+            console.error('Invalid data in DODO Response');
+            throw new ApiError(400, 'Invalid data')
+        } 
+    }
+
+    res.send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error processing webhook');
+  }
+});
+
+// Route to get total purchases count
+app.get('/purchase/count', async (req, res) => {
+  try {
+    const dodoPay = new DodoPaymentWrapper();
+
+    const count = await dodoPay.getAllPurchases();
+    res.json({ totalPurchases: count });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching purchase count');
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
