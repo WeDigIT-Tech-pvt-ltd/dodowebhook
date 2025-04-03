007defda3c1be510ad2aaab8ab51b15b30f5e3ca
Pro gupta <pchemistry.35@gmail.com>
2025-04-03 13:56:19 +0530

    chore: added env

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const _ = require('lodash');
-const dotenv = require('dotenv');
-const path = require('path');
 const { Webhook } = require("standardwebhooks");
 const DodoPaymentWrapper = require('./dodopayments');
-
-dotenv.config({ path: path.join(__dirname, './env') });
 
 const app = express();
 const PORT = 3000;
